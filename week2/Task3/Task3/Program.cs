514e2c341084078f056c7c3b94e81d7b9e864fa5
ynurtayev <yerzhannurtayev@mail.ru>
2019-02-10 15:56:11 +0600

    new changes

--- week2/Task3/Task3/Program.cs
+++ week2/Task3/Task3/Program.cs
@@ -9,9 +9,25 @@ namespace Task3
 {
     class Program
     {
+        static void printInfo(FileSystemInfo di ,int k)
+        {
+            string s = new string('\t', k);
+            s += di.Name;
+            Console.WriteLine(s);
+            if (di.GetType() == typeof(DirectoryInfo))
+            {
+                var v = (di as DirectoryInfo).GetFileSystemInfos();
+                foreach(var i in v)
+                {
+                    printInfo(i, k + 1);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            string  path=@
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\Ержан\Desktop\pp2");
+            printInfo(di, 0);
+            Console.ReadKey();
         }
     }
 }
--- week2/task1/task1/Program.cs
+++ week2/task1/task1/Program.cs
@@ -9,22 +9,25 @@ namespace task1
 {
     class Program
     {
-        static void Main(string[] args)
+        static bool pal(string s)//created method for checking palindrome
         {
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(s[i] != s[s.Length - i - 1])
+                {
+                    return false;
+                }
+
+            }return true;
+        }
+        static void Main(string[] args)
+        {  
             FileStream fs = new FileStream(@"C:\Users\Ержан\Desktop\pp2\week2\task1\input.txt", FileMode.Open, FileAccess.Read);
+            //to read input.txt in file
             StreamReader sr = new StreamReader(fs);
             string s = sr.ReadToEnd();
             Console.WriteLine(s);
-            bool ok = true;
-            for(int i = 0; i < s.Length; i++)
-            {
-                if (s[i] != s[s.Length - i - 1])
-                {
-                    ok =false;
-                    break;
-                }
-            }
-            if (ok==false)
+            if (pal(s))//checking 
             {
                 Console.Write("Yes");
             }
