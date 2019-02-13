45f50ff6aa0879801d8570fc1611a54e9a2a3cec
ynurtayev <yerzhannurtayev@mail.ru>
2019-02-13 20:15:42 +0600

    far manager

---
+++ week3/Task1/Task1/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class FarManager
+    {
+        public int cursor,sz;
+        public bool ok;
+        public string path;
+        DirectoryInfo di = null;
+        FileSystemInfo fsi = null;
+        public FarManager()
+        {
+            cursor = 0;
+        }
+        public FarManager(string path)
+        {
+            this.path = path;
+            cursor = 0;
+            di = new DirectoryInfo(path);
+            sz = di.GetFileSystemInfos().Length;
+            ok = true;
+        }
+        public void Color(FileSystemInfo fs,int index)
+        {
+            if (cursor == index)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                fsi = fs;
+            }
+            else if (fs.GetType() == typeof(DirectoryInfo))
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+            }
+        }
+        public void Show()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            di = new DirectoryInfo(path);
+            FileSystemInfo[] fs = di.GetFileSystemInfos();
+            for(int i = 0, k = 0; i < fs.Length; i++)
+            {
+                if(ok==false && fs[i].Name[0] == '.')
+                {
+                    continue;
+                }
+                Color(fs[i], k);
+                Console.WriteLine(fs[i].Name);
+                k++;
+            }
+        }
+        public void Up()
+        {
+            cursor--;
+            if (cursor < 0)
+                cursor = sz - 1;
+        }
+        public void Down()
+        {
+            cursor++;
+            if (cursor == 0)
+                cursor = 0;
+        }
+        public void CalcSz()
+        {
+            di = new DirectoryInfo(path);
+            FileSystemInfo[] fs = di.GetFileSystemInfos();
+            sz =fs.Length;
+            if (ok == false)
+            {
+                for(int i = 0; i < fs.Length; i++)
+                {
+                    if (fs[i].Name[0] == '.')
+                        sz--;
+                }
+            }
+        }
+        public void Start()
+        {
+            ConsoleKeyInfo conskey = Console.ReadKey();
+            while (conskey.Key != ConsoleKey.Escape)
+            {
+                CalcSz();
+                Show();
+                conskey = Console.ReadKey();
+                if (conskey.Key == ConsoleKey.UpArrow) Up();
+                if (conskey.Key == ConsoleKey.DownArrow) Down();
+                if (conskey.Key == ConsoleKey.RightArrow)
+                {
+                    ok = false;
+                    cursor = 0;
+                }
+                if (conskey.Key == ConsoleKey.LeftArrow)
+                {
+                    cursor=0;
+                    ok = true;
+                }
+                if (conskey.Key == ConsoleKey.Enter)
+                {
+                    if (fsi.GetType() == typeof(DirectoryInfo)){
+                        cursor = 0;
+                        path = fsi.FullName;
+                    }
+                }
+                if (conskey.Key == ConsoleKey.Backspace)
+                {
+                    cursor = 0;
+                    path=di.Parent.FullName;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Ержан\Desktop\pp2";
+            FarManager farManager = new FarManager(path);
+            farManager.Start();
+        }
+    }
+}
