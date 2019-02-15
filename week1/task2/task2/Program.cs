1ca9e5d3cb0bb573d558e91de375a592de9e588d
ynurtayev <yerzhannurtayev@mail.ru>
2019-02-15 23:57:31 +0600

    added comments

--- week1/task2/task2/Program.cs
+++ week1/task2/task2/Program.cs
@@ -6,34 +6,36 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    class Program
+    class Student//created class student
     {
-        class Student
+        string name;//declaring variables
+        public int year;
+        string id;
+        public Student(string name, string id)//constructor
         {
-          string name;
-          int id,year;
-            public Student(string name,int id)
-            {
-
-            }
-            public String aname()
-            {
-                return name;
-            }
-            public int aid()
-            {
-                return id;
-            }
-            public void ayear(int year)
-            {
-                ++year;
-            }
-
+            this.name = name;
+            this.id = id;
+        }
+        public String getName()//constructor that return name
+        {
+            return name;
+        }
+        public String getId()//constructor that return id
+        {
+            return id;
+        }
+        public int ayear(int year)//return impecment year
+        {
+            return year;
+            ++year;
         }
 
+
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
-
 
         }
     }
